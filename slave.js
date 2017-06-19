5a08e362c3ed7433d3a298695cb0939c2ffaaac6
The-Complex <willculhane3@gmail.com>
2017-06-18 23:13:12 -0600

    Extended fuse for self-destruction to 3 seconds from 1

--- slave.js
+++ slave.js
@@ -19,7 +19,7 @@ bot.on('message', message => {
   var speak = randInt(1,8);
   console.log('Speak (1 is active): ' + speak);
   if (message.author.username == 'SLAVE_1') {
-    delay(1000);
+    delay(3000);
     message.delete();
   };
   if (message.author.bot === false && message.content.length > 5 && speak == 1) {
